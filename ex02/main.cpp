df037393bae3b81cf5ff91296be1ad8e372abdd8
한예슬 <letmeshare46@gmail.com>
2022-10-19 15:42:31 +0900

    add test case

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,11 +7,22 @@
 
 int main() {
 	{
-		std::cout << "\n\n * ROUND 1:  * " << std::endl;
+		std::cout << "\n\n * ROUND 1: inherit from abstract class * " << std::endl;
 		// Animal meta;/* Error: object of abstract class type "Animal" is not allowed */
-		Cat nabi;
-		std::cout << "\nnabi: "; nabi.makeSound();
 		Dog baduk;
 		std::cout << "\nbaduk: "; baduk.makeSound();
+		Cat nabi;
+		std::cout << "\nnabi: "; nabi.makeSound();
+	}
+	{
+		std::cout << "\n\n * ROUND 2: pointer of abstract class * " << std::endl;
+		Animal* baduk = new Dog();
+		std::cout << "\nbaduk: "; baduk->makeSound();
+
+		Animal* nabi = new Cat();
+		std::cout << "\nnabi: "; nabi->makeSound();
+
+		delete baduk;
+		delete nabi;
 	}
 }
